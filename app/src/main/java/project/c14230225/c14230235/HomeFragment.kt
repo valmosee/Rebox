2db2b219daa813d3f33cc3b305b459eebdbc5a67
Timoty Darrel Davidson <c14230235@john.petra.ac.id>
2025-12-08 17:19:14 +0700

    Improved Home and User UI

--- app/src/main/java/project/c14230225/c14230235/HomeFragment.kt
+++ app/src/main/java/project/c14230225/c14230235/HomeFragment.kt
@@ -1,53 +1,51 @@
-package project.c14230225.c14230235
+    package project.c14230225.c14230235
 
-import android.content.Intent
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.GridLayoutManager
-import project.c14230225.c14230235.databinding.FragmentHomeBinding
+    import android.content.Context.MODE_PRIVATE
+    import android.content.SharedPreferences
+    import android.os.Bundle
+    import androidx.fragment.app.Fragment
+    import android.view.LayoutInflater
+    import android.view.View
+    import android.view.ViewGroup
+    import androidx.navigation.fragment.findNavController
+    import androidx.recyclerview.widget.GridLayoutManager
+    import project.c14230225.c14230235.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
-    var binding: FragmentHomeBinding? = null
+    class HomeFragment : Fragment() {
+        var binding: FragmentHomeBinding? = null
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-        val productList = listOf(
-            Product("Inspired Scarlett Extrait de Perfume", "Perfume", "Rp 10.000", "https://images.unsplash.com/photo-1526779259212-939e64788e3c?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D"),
-            Product("Celana Pendek SPORT Pria", "something", "Rp 12.000", "https://images.unsplash.com/photo-1526779259212-939e64788e3c?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D"),
-            Product("Ziyatta Kerudung Instan", "something", "Rp 10.965", "https://images.unsplash.com/photo-1526779259212-939e64788e3c?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D")
-            // add more
-        ).toMutableList()
+            val productList = listOf(
+                Product("Inspired Scarlett Extrait de Perfume", "Perfume", "Rp 10.000", "https://images.unsplash.com/photo-1526779259212-939e64788e3c?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D"),
+                Product("Celana Pendek SPORT Pria", "something", "Rp 12.000", "https://images.unsplash.com/photo-1526779259212-939e64788e3c?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D"),
+                Product("Ziyatta Kerudung Instan", "something", "Rp 10.965", "https://images.unsplash.com/photo-1526779259212-939e64788e3c?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D"),
+                Product("Ziyatta Kerudung Instan", "something", "Rp 10.965", "https://images.unsplash.com/photo-1526779259212-939e64788e3c?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D"),
+                // add more
+            ).toMutableList()
 
-        val adapter = productAdapter(productList)
+            val adapter = productAdapter(productList)
 
-        binding!!.rvProduct.layoutManager = GridLayoutManager(requireContext(), 4)
-        binding!!.rvProduct.adapter = adapter
+            binding!!.rvProduct.layoutManager = GridLayoutManager(requireContext(), 2)
+            binding!!.rvProduct.adapter = adapter
 
-        return binding!!.root
-    }
+            return binding!!.root
+        }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+//            binding!!.btnCart.setOnClickListener {
+//                findNavController().navigate(R.id.action_menuhome_to_uploadProductFragment)
+//            }
+        }
 
-        binding!!.btnTambah.setOnClickListener {
-            findNavController().navigate(R.id.action_menuhome_to_uploadProductFragment)
+        override fun onDestroyView() {
+            super.onDestroyView()
+            binding = null
         }
     }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        binding = null
-    }
-}
--- app/src/main/java/project/c14230225/c14230235/UserActivity.kt
+++ app/src/main/java/project/c14230225/c14230235/UserActivity.kt
@@ -1,10 +1,12 @@
 package project.c14230225.c14230235
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import project.c14230225.c14230235.databinding.ActivityUserBinding
@@ -17,6 +19,16 @@ class UserActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityUserBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val root = findViewById<View>(R.id.framecontainer)
+
+        ViewCompat.setOnApplyWindowInsetsListener(root) { view, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.updatePadding(
+                top = systemBars.top
+            )
+            insets
+        }
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.framecontainer) as NavHostFragment
